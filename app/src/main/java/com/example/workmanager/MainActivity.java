d9b04e15be7dd48fc6424fb61112c4e0b6b49e71
umaralyas@codesorbit.com <codesorbit.com>
2020-10-09 03:38:52 -0700

    Project Done

--- app/src/main/java/com/example/workmanager/MainActivity.java
+++ app/src/main/java/com/example/workmanager/MainActivity.java
@@ -2,30 +2,67 @@ package com.example.workmanager;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.work.Data;
 import androidx.work.OneTimeWorkRequest;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkInfo;
 import androidx.work.WorkManager;
 
 import com.example.workmanager.Worker.WorkHandler;
 
+import java.util.concurrent.TimeUnit;
+
+import static com.example.workmanager.Worker.WorkHandler.KEY_TASK_DESC;
+
 public class MainActivity extends AppCompatActivity {
+    public static final String TASK_DESC = "task_desc";
+    private Data data;
+    private TextView textView;
+    private OneTimeWorkRequest oneTimeWorkRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textViewStatus);
+        /**
+         * to pass data from views to worker class
+         */
+        data = new Data.Builder()
+                .putString(TASK_DESC, "Helo this is message")
+                .build();
 
-
-        final OneTimeWorkRequest oneTimeWorkRequest = new OneTimeWorkRequest.Builder(WorkHandler.class).build();
-
+        /**
+         * these are WorkRequest subclasses to perfrom tha task on time or periodically
+         */
+        oneTimeWorkRequest = new OneTimeWorkRequest.Builder(WorkHandler.class)
+                .setInputData(data)
+                .build();
+        /**
+         * Listner
+         */
         findViewById(R.id.btn_notify_message).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Helo", Toast.LENGTH_SHORT).show();
-                WorkManager.getInstance().enqueue(oneTimeWorkRequest);
+                WorkManager.getInstance().
+                        enqueue(oneTimeWorkRequest);
             }
         });
+
+        WorkManager.getInstance().getWorkInfoByIdLiveData(oneTimeWorkRequest.getId())
+                .observe(this, new Observer<WorkInfo>() {
+                    @Override
+                    public void onChanged(WorkInfo workInfo) {
+                        Data data1 = workInfo.getOutputData();
+                        String outPut = data1.getString(KEY_TASK_DESC);
+                        Toast.makeText(MainActivity.this, "" + outPut, Toast.LENGTH_SHORT).show();
+                        textView.append(workInfo.getState() + "\n");
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/workmanager/Worker/WorkHandler.java
+++ app/src/main/java/com/example/workmanager/Worker/WorkHandler.java
@@ -7,12 +7,17 @@ import android.os.Build;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
+import androidx.work.Data;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
 import com.example.workmanager.R;
 
+import static com.example.workmanager.MainActivity.TASK_DESC;
+
 public class WorkHandler extends Worker {
+    public static final String KEY_TASK_DESC = "key_task_desc";
+
     public WorkHandler(@NonNull Context context, @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
     }
@@ -20,11 +25,20 @@ public class WorkHandler extends Worker {
     @NonNull
     @Override
     public Result doWork() {
-        notificationData("WorkManager", "This Project is for testing");
-        return Result.success();
+        String taskDesk = getInputData().getString(TASK_DESC);
+        notificationData("WorkManager", taskDesk);
+        Data outputData = outPutData(KEY_TASK_DESC, "Hello There From Output");
+        return Result.success(outputData);
     }
 
-    private void notificationData(String title, String message) {
+    private Data outPutData(String key, String outputMessage) {
+        Data data = new Data.Builder()
+                .putString(key, outputMessage)
+                .build();
+        return data;
+    }
+
+    private void notificationData(String title, String task) {
         NotificationManager notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel notificationChannel = new NotificationChannel("codes", "codes", NotificationManager.IMPORTANCE_DEFAULT);
@@ -32,7 +46,7 @@ public class WorkHandler extends Worker {
         }
         NotificationCompat.Builder notificationCompat = new NotificationCompat.Builder(getApplicationContext(), "codes")
                 .setContentTitle(title)
-                .setContentText(message)
+                .setContentText(task)
                 .setSmallIcon(R.drawable.ic_launcher_background);
         notificationManager.notify(1, notificationCompat.build());
 
